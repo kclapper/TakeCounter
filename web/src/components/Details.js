6c75aabaeb3a9e6c0ecc68a9a02425f367e41ccd
Kyle Clapper <35474281+kclapper@users.noreply.github.com>
2023-07-17 08:54:07 -0700

    Updated client details styling

--- web/src/components/Details.js
+++ web/src/components/Details.js
@@ -10,26 +10,24 @@ export default function Details() {
     setDetails(event.target.value);
   }, [setDetails]);
 
-  return <div className="fixed-top">
-           <div className="container mt-2 pl-2 pr-2">
-             <div className="row no-gutters no-wrap justify-content-start align-items-stretch">
-               <div className="col-auto p-0 border-bottom">
-                 <h3 className="m-2">Client: </h3>
-               </div>
-               <div className="col-auto col-sm-6 p-0 ">
-                 <input type='text'
-                        value={ details }
-                        onChange={ handleInput }
-                        className="h3 text-white bg-transparent h-100 border-0 border-bottom"
-                        style={{
-                          outline: "none",
-                          overflow: "visible",
-                          width: details.length <= 25 ? "100%" : calcTextWidth(details)
-                        }} />
-               </div>
-               <div className="col d-none d-sm-block">
-               </div>
+  return <div className="fixed-top ms-4 me-4 mt-2 mb-0 m-sm-4">
+           <div className="d-flex justify-content-start align-items-stretch overflow-x-auto">
+             <div className="d-none d-sm-block" style={{ flexGrow: 0.25 }}/>
+             <div className="border-bottom">
+               <h3 className="m-2">Client: </h3>
              </div>
+             <div className="">
+               <input type='text'
+                      value={ details }
+                      onChange={ handleInput }
+                      className="h3 text-white bg-transparent h-100 border-0 border-bottom"
+                      style={{
+                        outline: "none",
+                        overflow: "visible",
+                        width: details.length <= 25 ? "100%" : calcTextWidth(details)
+                      }} />
+             </div>
+             <div className="d-none d-sm-block" style={{ flexGrow: 2 }}/>
            </div>
          </div>
 }
