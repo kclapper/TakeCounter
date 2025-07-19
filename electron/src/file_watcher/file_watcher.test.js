3d0414d8cedeaf71fb469d2e4b0cbb8d390aeeff
Kyle Clapper <35474281+kclapper@users.noreply.github.com>
2025-07-19 14:06:32 -0400

    Start and stop watching

--- electron/src/file_watcher/file_watcher.test.js
+++ electron/src/file_watcher/file_watcher.test.js
@@ -10,7 +10,6 @@ const endent = endent_formats.default;
 import { FileWatcher } from '.';
 
 test('write temp file', () => withLocalTmpDir(async () => {
-    console.log(endent);
     await outputFiles({
         'README.md': endent`
             # README
@@ -25,7 +24,57 @@ test('write temp file', () => withLocalTmpDir(async () => {
         `);
 }));
 
+test('start and stop watcher', () => withLocalTmpDir(async () => {
+    await outputFiles({
+        'Audio Files': {
+            'whatever.txt': `Some file to create the Audio Files folder`
+        }
+    });
 
-test('watch file and emit events', () => withLocalTmpDir(async () => {
-    const watcher = new FileWatcher();
+    const watcher = new FileWatcher('Audio Files');
+    watcher.watchTrackName('Audio 1');
+
+    expect(watcher.isWatching).toBe(true);
+
+    watcher.stopWatching();
+
+    expect(watcher.isWatching).toBe(false);
+}));
+
+test('change track to watch', () => withLocalTmpDir(async () => {
+
+}));
+
+test('change path to Audio Files', () => withLocalTmpDir(async () => {
+
+}));
+
+test('emit event when take changes', () => withLocalTmpDir(async () => {
+
+}));
+
+test('add and remove listeners', () => withLocalTmpDir(async () => {
+
+}));
+
+test.skip('get current take', () => withLocalTmpDir(async () => {
+    await outputFiles({
+        'Audio Files': {
+            'whatever.txt': `Some file to create the Audio Files folder`
+        }
+    });
+
+    const watcher = new FileWatcher('Audio Files');
+    watcher.watchTrackName('Audio 1');
+
+    await outputFiles({
+        'Audio Files': {
+            'Audio 1_01.wav': ``
+        }
+    });
+
+    expect(watcher.currentTake)
+        .toEqual(1);
+
+    watcher.stopWatching();
 }));
--- electron/src/file_watcher/index.js
+++ electron/src/file_watcher/index.js
@@ -1,8 +1,67 @@
+import path from 'node:path';
+import { watch } from 'node:fs/promises';
 
+export class FileWatcher extends EventTarget {
+    constructor(audioFilePath) {
+        super();
 
+        this.audioFilePath = audioFilePath;
+        this.isWatching = false;
+        this.watchAbort = new AbortController();
+    }
 
-export class FileWatcher {
-    constructor() {
+    get currentTake() {
+        return 0;
+    }
 
+    watchTrackName(trackName) {
+        this.trackName = trackName;
+
+        this.watchGenerator = watch(
+            this.audioFilePath, 
+            {  
+                signal: this.watchAbort.signal 
+            }
+        );
+
+        this.#nextWatchResult();
+        this.isWatching = true;
+    }
+
+    #nextWatchResult() {
+        this.currentWatchPromise = this.watchGenerator.next();
+        this.currentWatchPromise   
+            .then((watchIteratorResult) => {
+                this.#handleWatchIteratorResult(watchIteratorResult);
+            })
+            .catch((err) => {
+                if (err.name == 'AbortError') {
+                    return;
+                }
+                throw err;
+            });
+    }
+
+    #handleWatchIteratorResult(watchIteratorResult) {
+        if (watchIteratorResult.done) {
+            return;
+        }
+
+        const changeEvent = watchIteratorResult.value;
+        this.#handleAudioFilesChange(changeEvent);
+        this.#nextWatchResult();
+    }
+
+    #handleAudioFilesChange(changeEvent) {
+        console.log(changeEvent);
+    }
+
+    stopWatching() {
+        this.watchAbort.abort();
+        this.isWatching = false;
+    }
+
+    async nextUpdate() {
+        await this.currentWatchPromise;
     }
 }
