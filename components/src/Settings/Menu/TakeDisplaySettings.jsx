fe765c576f5cbad4968f71bf86833b19a8e82a77
Ben Miller <ben.db.miller@gmail.com>
2025-08-10 19:32:33 -0400

    hi kyle 4

--- components/src/Settings/Menu/TakeDisplaySettings.jsx
+++ components/src/Settings/Menu/TakeDisplaySettings.jsx
@@ -4,27 +4,25 @@ import { useSetting } from '..';
 import { TextItem , BooleanItem } from '../Items/index';
 
 export function TakeDisplaySettings() {
-const [takeTextPrefix, setTakePrefix] = useSetting( 'takeDisplaySettings', 'takeTextPrefix' );
-const [showTakePrefix, setShowTakePrefix] = useSetting( 'takeDisplaySettings', 'showTakePrefix' );
-const [showTakeButtons, setShowTakeButtons] = useSetting( 'takeDisplaySettings', 'showTakeButtons' );
+    const [takeTextPrefix, setTakePrefix] = useSetting( 'takeDisplaySettings', 'takeTextPrefix' );
+    const [showTakePrefix, setShowTakePrefix] = useSetting( 'takeDisplaySettings', 'showTakePrefix' );
+    const [showTakeButtons, setShowTakeButtons] = useSetting( 'takeDisplaySettings', 'showTakeButtons' );
 
-   return (
+    return (
         <div>
             <h4 className='row border-bottom mt-3'>
                 Take Display
             </h4>
             <TextItem name='Take Display Prefix'
-                value={ takeTextPrefix }
-                onChange={ setTakePrefix }> 
+                      value={ takeTextPrefix }
+                      onChange={ setTakePrefix }> 
             </TextItem>
             <BooleanItem name = 'Show Take Prefix'
-                value={ showTakePrefix }
-                onChange={ setShowTakePrefix }
-                />
+                         value={ showTakePrefix }
+                         onChange={ setShowTakePrefix }/>
             <BooleanItem name = 'Show Take Buttons'
-                value={ showTakeButtons }
-                onChange={ setShowTakeButtons }
-                />
-            </div>
-            );
+                         value={ showTakeButtons }
+                         onChange={ setShowTakeButtons }/>
+        </div>
+    );
 }
